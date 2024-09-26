e68eeb1b53cf366a13fb787a335274b792772c66
keithwalsh <keithwalsh@gmail.com>
2024-09-26 23:35:25 +0100

    chore: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,25 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePaths: ['<rootDir>/src'],
   roots: ['<rootDir>/src', '<rootDir>/test'],
-  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  transformIgnorePatterns: [
-    '/node_modules/', // Ignore transforming files in node_modules
-    // Add other patterns here if needed
-  ],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', 'jest.setup.ts'],
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/index.tsx',
-    '!src/react-app-env.d.ts',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.js$'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+      babelConfig: true,
     },
   },
 };
