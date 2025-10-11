e850cf8ff0658d9e7ec83ac4cfdfeaba4c4aec5f
keithwalsh <keithwalsh@gmail.com>
2025-10-11 03:17:46 +0100

    chore: bump version to 1.3.7, update React and TypeScript dependencies, and remove unused files

--- dist/utils.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview Utility functions and classes for generating formatted markdown
- * table strings with alignment, padding, and column width adjustments.
- */
-import type { InputData, Alignment } from './types';
-export declare function generateMarkdownTableString(inputData: InputData, columnAlignments: readonly Alignment[], canAdjustColumnWidths?: boolean, useTabs?: boolean, replaceNewlines?: boolean, hasPadding?: boolean): string;
-export declare function generateAlphabetHeaders(columnCount: number): string[];
-//# sourceMappingURL=utils.d.ts.map
